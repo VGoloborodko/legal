5bb914599a7236f5ad21e27ff786964477a8fcaf
VGoloborodko <goloborodko.vitali@gmail.com>
2026-04-27 15:51:54 +0300

    fix: убран vercelPreset

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import { reactRouter } from '@react-router/dev/vite'
-import { vercelPreset } from '@vercel/react-router'
 
 export default defineConfig({
   plugins: [
-    reactRouter({
-      presets: [vercelPreset()],
-    }),
+    reactRouter(),
   ],
 })
