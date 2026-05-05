3575df54a5f71cf9998d4e509e80f17be6943238
VGoloborodko <goloborodko.vitali@gmail.com>
2026-05-05 17:02:21 +0300

    +hero

---
+++ app/components/hero/Hero.tsx
@@ -0,0 +1,10 @@
+import styles from './Hero.module.scss';
+
+export default function Navbar() {
+  return (
+    <section className={styles['hero']}>
+        {/* <div className=></div> */}
+      <div className="container"></div>
+    </section>
+  );
+}
